2730edbb662df3179fd6cb4156b8f51a184b4689
nikpan1 <nikodem.panknin@gmail.com>
2024-05-12 21:00:38 +0200

    final form of GUI, custom widget setup

--- build/qt-smallboy_autogen/include/ui_mainwindow.h
+++ build/qt-smallboy_autogen/include/ui_mainwindow.h
@@ -12,13 +12,18 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QFrame>
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
+#include <playerwidget.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -27,45 +32,168 @@ class Ui_MainWindow
 public:
     QAction *actionExit;
     QWidget *centralWidget;
+    QGridLayout *gridLayout_3;
+    QSplitter *splitter;
+    QWidget *gridLayoutWidget;
     QGridLayout *gridLayout;
+    QPushButton *AddNewPlayerW;
+    QSpinBox *PlayerAmountW;
+    QComboBox *PlayerTypeW;
     QLabel *label;
-    QMenuBar *menuBar;
-    QMenu *menuFile;
-    QStatusBar *statusBar;
+    QWidget *gridLayoutWidget_2;
+    QGridLayout *gridLayout_2;
+    QPushButton *PlayRoundW;
+    QFrame *line_2;
+    QLabel *roundNumLabelW;
+    QFrame *line;
+    QLabel *label_3;
+    QPushButton *StartGameW;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout_5;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QVBoxLayout *verticalLayout_2;
+    playerwidget *frame;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(243, 111);
+        MainWindow->resize(820, 870);
         actionExit = new QAction(MainWindow);
         actionExit->setObjectName(QString::fromUtf8("actionExit"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
-        gridLayout = new QGridLayout(centralWidget);
+        centralWidget->setEnabled(true);
+        gridLayout_3 = new QGridLayout(centralWidget);
+        gridLayout_3->setSpacing(6);
+        gridLayout_3->setContentsMargins(11, 11, 11, 11);
+        gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
+        splitter = new QSplitter(centralWidget);
+        splitter->setObjectName(QString::fromUtf8("splitter"));
+        splitter->setOrientation(Qt::Vertical);
+        gridLayoutWidget = new QWidget(splitter);
+        gridLayoutWidget->setObjectName(QString::fromUtf8("gridLayoutWidget"));
+        gridLayout = new QGridLayout(gridLayoutWidget);
         gridLayout->setSpacing(6);
         gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
-        label = new QLabel(centralWidget);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setPixmap(QPixmap(QString::fromUtf8(":/resources/helloworld-transparent.png")));
-        label->setAlignment(Qt::AlignCenter);
+        gridLayout->setContentsMargins(0, 0, 0, 0);
+        AddNewPlayerW = new QPushButton(gridLayoutWidget);
+        AddNewPlayerW->setObjectName(QString::fromUtf8("AddNewPlayerW"));
 
-        gridLayout->addWidget(label, 1, 0, 1, 1);
+        gridLayout->addWidget(AddNewPlayerW, 1, 2, 1, 1);
+
+        PlayerAmountW = new QSpinBox(gridLayoutWidget);
+        PlayerAmountW->setObjectName(QString::fromUtf8("PlayerAmountW"));
+        PlayerAmountW->setMinimum(1);
+
+        gridLayout->addWidget(PlayerAmountW, 1, 0, 1, 1);
+
+        PlayerTypeW = new QComboBox(gridLayoutWidget);
+        PlayerTypeW->setObjectName(QString::fromUtf8("PlayerTypeW"));
+
+        gridLayout->addWidget(PlayerTypeW, 1, 1, 1, 1);
+
+        label = new QLabel(gridLayoutWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy);
+        label->setLineWidth(1);
+        label->setTextFormat(Qt::PlainText);
+
+        gridLayout->addWidget(label, 0, 0, 1, 1);
+
+        splitter->addWidget(gridLayoutWidget);
+        gridLayoutWidget_2 = new QWidget(splitter);
+        gridLayoutWidget_2->setObjectName(QString::fromUtf8("gridLayoutWidget_2"));
+        gridLayout_2 = new QGridLayout(gridLayoutWidget_2);
+        gridLayout_2->setSpacing(6);
+        gridLayout_2->setContentsMargins(11, 11, 11, 11);
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        gridLayout_2->setContentsMargins(0, 0, 0, 0);
+        PlayRoundW = new QPushButton(gridLayoutWidget_2);
+        PlayRoundW->setObjectName(QString::fromUtf8("PlayRoundW"));
+
+        gridLayout_2->addWidget(PlayRoundW, 3, 0, 1, 1);
+
+        line_2 = new QFrame(gridLayoutWidget_2);
+        line_2->setObjectName(QString::fromUtf8("line_2"));
+        line_2->setFrameShape(QFrame::HLine);
+        line_2->setFrameShadow(QFrame::Sunken);
+
+        gridLayout_2->addWidget(line_2, 6, 0, 1, 1);
+
+        roundNumLabelW = new QLabel(gridLayoutWidget_2);
+        roundNumLabelW->setObjectName(QString::fromUtf8("roundNumLabelW"));
+        sizePolicy.setHeightForWidth(roundNumLabelW->sizePolicy().hasHeightForWidth());
+        roundNumLabelW->setSizePolicy(sizePolicy);
+        roundNumLabelW->setAutoFillBackground(false);
+        roundNumLabelW->setTextFormat(Qt::PlainText);
+        roundNumLabelW->setAlignment(Qt::AlignHCenter|Qt::AlignTop);
+
+        gridLayout_2->addWidget(roundNumLabelW, 4, 0, 1, 1);
+
+        line = new QFrame(gridLayoutWidget_2);
+        line->setObjectName(QString::fromUtf8("line"));
+        line->setFrameShape(QFrame::HLine);
+        line->setFrameShadow(QFrame::Sunken);
+
+        gridLayout_2->addWidget(line, 0, 0, 1, 1);
+
+        label_3 = new QLabel(gridLayoutWidget_2);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        sizePolicy.setHeightForWidth(label_3->sizePolicy().hasHeightForWidth());
+        label_3->setSizePolicy(sizePolicy);
+        label_3->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignTop);
+
+        gridLayout_2->addWidget(label_3, 5, 0, 1, 1);
+
+        StartGameW = new QPushButton(gridLayoutWidget_2);
+        StartGameW->setObjectName(QString::fromUtf8("StartGameW"));
+
+        gridLayout_2->addWidget(StartGameW, 1, 0, 1, 1);
+
+        splitter->addWidget(gridLayoutWidget_2);
+        verticalLayoutWidget = new QWidget(splitter);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayout_5 = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout_5->setSpacing(6);
+        verticalLayout_5->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_5->setObjectName(QString::fromUtf8("verticalLayout_5"));
+        verticalLayout_5->setContentsMargins(0, 0, 0, 0);
+        scrollArea = new QScrollArea(verticalLayoutWidget);
+        scrollArea->setObjectName(QString::fromUtf8("scrollArea"));
+        scrollArea->setMinimumSize(QSize(0, 450));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QString::fromUtf8("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 798, 28));
+        sizePolicy.setHeightForWidth(scrollAreaWidgetContents->sizePolicy().hasHeightForWidth());
+        scrollAreaWidgetContents->setSizePolicy(sizePolicy);
+        verticalLayout_2 = new QVBoxLayout(scrollAreaWidgetContents);
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        frame = new playerwidget(scrollAreaWidgetContents);
+        frame->setObjectName(QString::fromUtf8("frame"));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+
+        verticalLayout_2->addWidget(frame);
+
+        scrollArea->setWidget(scrollAreaWidgetContents);
+
+        verticalLayout_5->addWidget(scrollArea);
+
+        splitter->addWidget(verticalLayoutWidget);
+
+        gridLayout_3->addWidget(splitter, 0, 0, 1, 1);
 
         MainWindow->setCentralWidget(centralWidget);
-        menuBar = new QMenuBar(MainWindow);
-        menuBar->setObjectName(QString::fromUtf8("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 243, 21));
-        menuFile = new QMenu(menuBar);
-        menuFile->setObjectName(QString::fromUtf8("menuFile"));
-        MainWindow->setMenuBar(menuBar);
-        statusBar = new QStatusBar(MainWindow);
-        statusBar->setObjectName(QString::fromUtf8("statusBar"));
-        MainWindow->setStatusBar(statusBar);
-
-        menuBar->addAction(menuFile->menuAction());
-        menuFile->addAction(actionExit);
 
         retranslateUi(MainWindow);
 
@@ -74,10 +202,14 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Hello, World!", nullptr));
+        MainWindow->setProperty("maly-dresiarz", QVariant(QCoreApplication::translate("MainWindow", "Hello, World!", nullptr)));
         actionExit->setText(QCoreApplication::translate("MainWindow", "E&xit", nullptr));
-        label->setText(QString());
-        menuFile->setTitle(QCoreApplication::translate("MainWindow", "&File", nullptr));
+        AddNewPlayerW->setText(QCoreApplication::translate("MainWindow", "Add New Player", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Add new Players", nullptr));
+        PlayRoundW->setText(QCoreApplication::translate("MainWindow", "Play Round", nullptr));
+        roundNumLabelW->setText(QCoreApplication::translate("MainWindow", "ROUND: 1", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Players", nullptr));
+        StartGameW->setText(QCoreApplication::translate("MainWindow", "Start Game", nullptr));
     } // retranslateUi
 
 };
---
+++ playerwidget.h
@@ -0,0 +1,60 @@
+#include <QWidget>
+#include <QFrame>
+#include <QFormLayout>
+#include <QLabel>
+#include <QComboBox>
+#include <QPushButton>
+
+
+class playerwidget : public QFrame {
+    Q_OBJECT
+
+public:
+    explicit playerwidget(QWidget *parent = nullptr) : QFrame(parent) {
+        setupUi();
+    }
+
+    // Getters for accessing widget components
+    QLabel* playerNameLabel() const { return playerName; }
+    QLabel* playerNumLabel() const { return playerNum; }
+    QLabel* emptyLabelWidget() const { return emptyLabel; } // Renamed member function
+    QComboBox* playerMoveComboBox() const { return playerMove; }
+    QPushButton* removePlayerButton() const { return removePlayer; }
+
+private:
+    QLabel *playerName;
+    QLabel *playerNum;
+    QLabel *emptyLabel;
+    QComboBox *playerMove;
+    QPushButton *removePlayer;
+
+    void setupUi() {
+        QFormLayout *layout = new QFormLayout(this);
+
+        playerName = new QLabel(this);
+        playerNum = new QLabel(this);
+        emptyLabel = new QLabel(this);
+        playerMove = new QComboBox(this);
+        playerMove->addItem("a");
+        playerMove->addItem("b");
+        playerMove->addItem("c");
+        playerMove->addItem("d");
+        removePlayer = new QPushButton("Remove Player", this);
+
+        layout->addRow("Player Name:", playerName);
+        layout->addRow("Player Number:", playerNum);
+        layout->addRow("Empty:", emptyLabel);
+        layout->addRow("Player Move:", playerMove);
+        layout->addRow(removePlayer);
+
+        // Add additional labels in the second column
+        layout->addRow("Zasoby:", new QLabel(this));
+        layout->addRow("Szacun:", new QLabel(this));
+        layout->addRow("Kasa:", new QLabel(this));
+        layout->addRow("BMW:", new QLabel(this));
+        layout->addRow("Haracz:", new QLabel(this));
+
+        setLayout(layout);
+
+    }
+};
